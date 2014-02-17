2da71db694b6a566db0580c37b2b6ca3faebea4e
Marten Nylen <c.marten.nylen@gmail.com>
2014-02-17 19:07:29 +0100

    Fix for add game.

--- public/app/admin/AdminCtrl.js
+++ public/app/admin/AdminCtrl.js
@@ -1,10 +1,10 @@
-﻿app.controller('AdminCtrl',  ['$scope', '$http', '$location', 'consoles', 'baseRegions', 'user', function ($scope, $http, $location, consoles, baseRegions, user) {
+﻿app.controller('AdminCtrl',  ['$scope', '$http', '$location', 'consoles', 'baseRegions', 'user', function($scope, $http, $location, consoles, baseRegions, user) {
     $scope.user = user;
     $scope.consoles = consoles;
     $scope.regions = baseRegions;
-    $scope.game = { type: 'game', regions: {}, attr: { extras: [] } };
+    $scope.game = { type: 'game', regions: {}, variants: [] };
     $scope.postMessage = '';
-    $scope.attributes = [{ id: 'c', longName: 'Kassett', selected: true }, { id: 'i', longName: 'Manual', selected: true }, { id: 'b', longName: 'Kartong', selected: true }];
+    $scope.currentVariant = { desc: '', attr: { common: [{ id: 'c', desc: '', longName: 'Kassett', selected: true }, { id: 'i', desc: '', longName: 'Manual', selected: true }, { id: 'b', desc: '', longName: 'Kartong', selected: true }], extras: [] } };
     $scope.currentExtra = '';
 
     $scope.currentRegions = {
@@ -16,20 +16,32 @@
         $scope.currentRegions.sub = $scope.currentRegions.main.regions[0];
     };
 
-    $scope.handleExtra = function (extra) {
-        if (_.contains($scope.game.attr.extras, extra)) {
-            $scope.game.attr.extras.splice(_.indexOf($scope.game.attr.extras, extra), 1);
+    $scope.handleVariant = function (variant) {
+        if (_.contains($scope.game.variants, variant)) {
+            $scope.game.variants.splice(_.indexOf($scope.game.variants, variant), 1);
         } else {
-            $scope.game.attr.extras.push({'name': extra });
+            $scope.game.variants.push(variant);
+        }
+
+        $scope.currentVariant = { desc: '', attr: { common: [{ id: 'c', desc: '', longName: 'Kassett', selected: true }, { id: 'i', desc: '', longName: 'Manual', selected: true }, { id: 'b', desc: '', longName: 'Kartong', selected: true }], extras: [] } };
+    };
+    
+    $scope.handleExtra = function (extra) {
+        if (_.contains($scope.currentVariant.attr.extras, extra)) {
+            $scope.currentVariant.attr.extras.splice(_.indexOf($scope.currentVariant.attr.extras, extra), 1);
+        } else {
+            $scope.currentVariant.attr.extras.push({ 'name': extra });
         }
 
         $scope.currentExtra = '';
     };
 
     $scope.addGame = function () {
-        $scope.game.attr.common = _.pluck(_.filter($scope.attributes, function (c) {
-            return c.selected;
-        }), 'id');
+        _.each($scope.game.variants, function(v) {
+            v.attr.common = _.map(v.attr.common, function(a) {
+                return { id: a.id, desc: a.desc, };
+            });
+        });
 
         $scope.game.regions.main = $scope.currentRegions.main.id;
         $scope.game.regions.sub = [$scope.currentRegions.sub.id];
@@ -38,7 +50,7 @@
         $http.post('/api/newgame', angular.toJson($scope.game)).
         success(function (response) {
             $scope.postMessage = 'Spel sparat';
-            $scope.game = { type: 'game', console: consoles[0].id, regions: {}, attr: { extras: [] } };
+            $scope.game = { type: 'game', console: consoles[0].id, regions: {}, variants: [] };
         }).
         error(function (id) {
             console.log('nej nej NEJ');
@@ -46,6 +58,6 @@
     };
 
     $scope.validateFields = function () {
-        return $scope.addGameForm.$valid;
+        return $scope.addGameForm.$valid && $scope.game.variants.length;
     };
 }]);
