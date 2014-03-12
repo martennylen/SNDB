fe8ec133f2f7a137430e43699a7e7bc52ba37d44
Marten Nylen <c.marten.nylen@gmail.com>
2014-03-12 19:14:28 +0100

    Region grouping.

--- public/app/game/GameRegionCtrl.js
+++ public/app/game/GameRegionCtrl.js
@@ -1,6 +1,7 @@
 ﻿app.controller('GameRegionCtrl', ['$scope', '$location', '$stateParams', '$rootScope',
     function ($scope, $location, $stateParams, $rootScope) {
         console.log('gameregion');
+        //console.log($rootScope.consoles);
         $scope.apa = $rootScope.consoles;
 
         $scope.consoleName = $stateParams.consoleName;
@@ -19,16 +20,18 @@
 
         $scope.regions = _.find($scope.apa, function (c) {
                 return c.id === $scope.consoleName;
-            }).regions;
+        }).regions;
+
+        console.log($scope.regions);
 
             $scope.currentRegion.region =
                 _.find($scope.regions, function (r) {
                     return r.id === $scope.regionName;
                 });
 
-            $scope.subRegions = $scope.currentRegion.region.subRegions;
+            //$scope.subRegions = $scope.currentRegion.region.subRegions;
             
-            $scope.currentRegion.subRegion = _.find($scope.subRegions, function (sr) {
+            $scope.currentRegion.subRegion = _.find($scope.currentRegion.region.subRegions, function (sr) {
                 return sr.id === $scope.subRegionName;
             });
        
@@ -36,8 +39,8 @@
         $location.path('/' + $stateParams.consoleName + '/' + $scope.currentRegion.region.id + '/' + $scope.currentRegion.region.subRegions[0].id).replace();
     };
 
-    $scope.subRegionChanged = function (sr) {
-        $location.path('/' + $stateParams.consoleName + '/' + $scope.currentRegion.region.id + '/' + sr.id).replace();
+    $scope.subRegionChanged = function (r, sr) {
+        $location.path('/' + $stateParams.consoleName + '/' + r.id + '/' + sr.id).replace();
     };
         
     //$scope.$on('PUNG', function (event, data) {
